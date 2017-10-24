1b4290d4dd57ee90ed2742e7ce3745c8f0eb4d39
Vlad <fiend3d@ya.ru>
2017-10-25 02:41:53 +0300

    add settings

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const {app, Menu, Tray} = require('electron')
+const {app, globalShortcut, Menu, Tray} = require('electron')
 const path = require('path')
+const jsonfile = require('jsonfile')
 
 const icon_path = path.join(__dirname, 'assets', 'icon.ico')
+const settings_path = path.join(__dirname, 'settings.json')
+
 let tray = null
+let settings = jsonfile.readFileSync(settings_path)
 
 app.on('ready', () => {
   tray = new Tray(icon_path)
@@ -14,4 +18,10 @@ app.on('ready', () => {
   ])
   tray.setToolTip('RegEx Clipboard')
   tray.setContextMenu(contextMenu)
+
+  for (let setting of settings) {
+    globalShortcut.register(setting['key'], () => {
+        console.log(setting['key'] + ' is pressed')
+    })
+  }
 })
